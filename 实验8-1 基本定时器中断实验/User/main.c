0b1974019216705d49b5629ca837dc3a6362ebf6
Magicial-PAL <13826075703@163.com>
2024-10-20 20:28:13 +0800

    add a key detect stm32 project by siganl filter function

---
+++ 实验8-1 基本定时器中断实验/User/main.c
@@ -0,0 +1,73 @@
+/**
+ ****************************************************************************************************
+ * @file        main.c
+ * @author      正点原子团队(ALIENTEK)
+ * @version     V1.0
+ * @date        2020-04-22
+ * @brief       基本定时器中断 实验
+ * @license     Copyright (c) 2020-2032, 广州市星翼电子科技有限公司
+ ****************************************************************************************************
+ * @attention
+ *
+ * 实验平台:正点原子 STM32F103开发板
+ * 在线视频:www.yuanzige.com
+ * 技术论坛:www.openedv.com
+ * 公司网址:www.alientek.com
+ * 购买地址:openedv.taobao.com
+ *
+ ****************************************************************************************************
+ */
+
+#include "./SYSTEM/sys/sys.h"
+#include "./SYSTEM/usart/usart.h"
+#include "./SYSTEM/delay/delay.h"
+#include "./BSP/LED/led.h"
+#include "./BSP/TIMER/btim.h"
+#include "./BSP/KEY/key.h"
+
+
+int main(void)
+{
+    HAL_Init();                             /* 初始化HAL库 */
+    sys_stm32_clock_init(RCC_PLL_MUL9);     /* 设置时钟, 72Mhz */
+    delay_init(72);                         /* 延时初始化 */
+    usart_init(115200);                     /* 串口初始化为115200 */
+    led_init();                             /* 初始化LED */
+		key_init();
+    btim_timx_int_init(10 - 1, 7200 - 1);   // 1MS
+
+    while (1)
+    {
+				if(flag_100ms == 1)
+				{
+						flag_100ms = 0;
+					
+//						LED0_TOGGLE();
+						
+				}
+				
+				if(flag_20ms == 1)
+				{
+						flag_20ms = 0;
+					
+						if(SignalFilter(KEY0, &signal_filter1) == 1)
+						{
+								if(signal_filter1.last_state == 1)
+								{
+										LED0_TOGGLE();
+								}
+								else
+								{
+										LED1_TOGGLE();
+								}
+								
+						}
+				}
+				
+				if(flag_1ms == 1)
+				{
+						flag_1ms = 0;
+					
+				}				
+    }
+}
